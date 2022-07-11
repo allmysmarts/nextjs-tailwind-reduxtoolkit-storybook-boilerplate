7918d22ba89f642290b2de4f41a7c3e2d6285161
unbreakablery <unbreakablery@gmail.com>
2022-07-12 02:11:43 +0900

    fix: storybook conf

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,7 @@
+import { Provider as ReduxProvider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import store, { persistor } from 'store';
+
 import 'assets/sass/style.scss';
 import 'assets/sass/tailwind.scss';
 
@@ -14,12 +18,47 @@ if (typeof global.process === 'undefined') {
   worker.start();
 }
 
+const BREAKPOINTS_INT = {
+  xs: 375,
+  sm: 600,
+  md: 900,
+  lg: 1200,
+  xl: 1536,
+};
+
+const customViewports = Object.fromEntries(
+  Object.entries(BREAKPOINTS_INT).map(([key, val], idx) => {
+    console.log(val);
+    return [
+      key,
+      {
+        name: key,
+        styles: {
+          width: `${val}px`,
+          height: `${(idx + 5) * 10}vh`,
+        },
+      },
+    ];
+  })
+);
+
 const OriginalNextImage = NextImage.default;
 
 Object.defineProperty(NextImage, 'default', {
   configurable: true,
   value: props => <OriginalNextImage {...props} unoptimized />,
 });
+
+export const decorators = [
+  Story => (
+    <ReduxProvider store={store}>
+      {/*@ts-ignore TYPE NEEDS FIXING*/}
+      <PersistGate persistor={persistor}>
+        <Story />
+      </PersistGate>
+    </ReduxProvider>
+  ),
+];
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
@@ -29,4 +68,6 @@ export const parameters = {
       date: /Date$/,
     },
   },
+  viewport: { viewports: customViewports },
+  layout: 'fullscreen',
 };
--- stories/pages/home.stories.jsx
+++ stories/pages/home.stories.jsx
@@ -1,16 +1,8 @@
-import Home, { getServerSideProps } from '../../pages/index';
+import Home from '../../pages/index';
 
 export default {
   title: 'Pages/Home',
   component: Home,
 };
 
-export const HomePage = (args, { loaded: { name } }) => <Home {...args} name={name} />;
-HomePage.args = { name: 'John Dough' };
-HomePage.loaders = [
-  async () => {
-    let data = await getServerSideProps();
-
-    return data.props;
-  },
-];
+export const HomePage = args => <Home {...args} />;
